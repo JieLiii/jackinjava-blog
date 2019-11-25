78e64a2a1079f11a60cf4659c014d7c92003c459
venus_jieli <jie.li@venusource.com>
2019-11-25 15:26:28 +0800

    初始提交

---
+++ src/main/java/com/jackinjava/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.jackinjava.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiJie on 2019/11/25
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
