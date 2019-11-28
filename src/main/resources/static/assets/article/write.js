b3d69b967b34b7f7240512248702dc9420839975
venus_jieli <jie.li@venusource.com>
2019-11-28 17:20:47 +0800

    调整文章编辑页面样式

--- src/main/resources/static/assets/article/write.js
+++ src/main/resources/static/assets/article/write.js
@@ -1,10 +1,18 @@
 new Vue({
   el: '#app',
   created: function () {
+
   },
   mounted: function () {
+    var iframe = parent.document.getElementById('menu-iframe');
+    var mainHeight = iframe.offsetHeight;
+    var mainWidth = iframe.offsetWidth;
+    this.contentStyle.height = mainHeight-16 + 'px';
+    this.editor.height = mainHeight-53 + 'px';
+    this.editorStyle.width = mainWidth - 520 + 'px';
+
     var E = window.wangEditor;
-    var editor = new E(this.$refs.editor);
+    var editor = new E(this.$refs.toolbar,this.$refs.editor);
     editor.customConfig.debug = true;
     editor.create();
     editor.txt.html('<p>hello</p>');
@@ -12,13 +20,32 @@ new Vue({
   },
   data: function () {
     return {
-      E: {}
+      E: {},
+      isCollapse: true,
+      contentStyle: {
+        height: '0'
+      },
+      editorStyle: {
+        width: '0'
+      },
+      editor: {
+        height: '0'
+      }
     }
   },
   methods: {
-    getContent: function () {
+    save: function () {
       alert(this.E.txt.text());
       alert(this.E.txt.html());
+    },
+    handleOpen: function(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose: function(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    goBack: function () {
+      window.location.href = ctxPath + '/nav/article/index.html';
     }
   }
 })
--- src/main/resources/static/assets/index.js
+++ src/main/resources/static/assets/index.js
@@ -2,19 +2,21 @@ new Vue({
   el: '#app',
   created: function () {
     this.appStyle.height = Math.max(document.body.scrollHeight,document.documentElement.scrollHeight)-74  + 'px';
-    //this.iframeHeight = Math.max(document.body.scrollHeight,document.documentElement.scrollHeight) - 100;
+    this.iframeHeight = Math.max(document.body.scrollHeight,document.documentElement.scrollHeight) - 65;
     if(this.activeIndex === '1'){
       this.activeSrc = ctxPath + '/nav/homepage/index.html';
+    } else if(this.activeIndex === '3'){
+      this.activeSrc = ctxPath + '/nav/article/index.html';
     }
   },
   data: function() {
     return {
-      activeIndex: '1',
+      activeIndex: '3',
       activeSrc: '',
       appStyle: {
         height: '0'
       },
-      iframeHeight: '100%'
+      iframeHeight: '0'
     };
   },
   methods: {
